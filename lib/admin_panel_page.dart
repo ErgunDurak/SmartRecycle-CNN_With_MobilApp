05fe7729099357dccf3e414558b8c325f165deca
ErgunDurak <156503314+ErgunDurak@users.noreply.github.com>
2026-01-16 19:03:29 +0300

    mesaj

--- lib/admin_panel_page.dart
+++ lib/admin_panel_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//deneme123
 class AdminPanelPage extends StatelessWidget {
   const AdminPanelPage({super.key});
 
